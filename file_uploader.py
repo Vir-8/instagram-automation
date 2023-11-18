a502dafc68e4cf8457e0f4603164308355785842
Vir <73329256+Vir-8@users.noreply.github.com>
2023-11-18 14:53:36 +0530

    Fix reel privacy popup order

--- file_uploader.py
+++ file_uploader.py
@@ -17,17 +17,32 @@ class FileUploader:
             f"file:///{VIDEO_PATH_ON_DEVICE}{self.file[len('videos/'):][2:]}"
         )
         time.sleep(3)
-        print(local_video_path)
         return local_video_path
+
+    def upload_story(self, d, path):
+        d.shell(
+            f"am start -a android.intent.action.SEND -t video/* --eu android.intent.extra.STREAM {path} com.instagram.android"
+        )
+        d(text="Stories").click()
+        time.sleep(2)
+
+        if d(resourceId="android:id/button_once").exists():
+            d(resourceId="android:id/button_once").click()
+
+        print("uploaded")
 
     def upload_reel(self, d, path):
         d.shell(
             f"am start -a android.intent.action.SEND -t video/* --eu android.intent.extra.STREAM {path} com.instagram.android"
         )
-        d(text="Reels").click()
-        d(resourceId="android:id/button_once").click()  # Click "Just Once"
+        if d(text="Reels").exists():
+            d(text="Reels").click()
+        else:
+            d(description="Reels").click()
 
-        time.sleep(2)
+        if d(resourceId="android:id/button_once").exists():
+            d(resourceId="android:id/button_once").click()
+        time.sleep(12)
 
         # Popup that says "videos will be uploaded as reels instead of posts"
         if d(text="OK", resourceId="com.instagram.android:id/primary_action").exists():
@@ -45,6 +60,13 @@ class FileUploader:
             ).click()
             time.sleep(2)
 
+        time.sleep(6)
+
+        # upload the reel
+        if d(resourceId="com.instagram.android:id/share_button").exists():
+            d(resourceId="com.instagram.android:id/share_button").click()
+            time.sleep(2)
+
         # Popup about privacy, reels being visible to others
         if d(
             resourceId="com.instagram.android:id/clips_download_privacy_nux_button"
@@ -53,10 +75,23 @@ class FileUploader:
                 resourceId="com.instagram.android:id/clips_download_privacy_nux_button"
             ).click()
             time.sleep(2)
-
-        # Finally upload the reel
-        if d(resourceId="com.instagram.android:id/share_button").exists():
-            d(resourceId="com.instagram.android:id/share_button").click()
-            time.sleep(2)
+        elif d(
+            resourceId="com.instagram.android:id/clips_nux_sheet_share_button"
+        ).exists():
+            print("here")
+            d(
+                resourceId="com.instagram.android:id/clips_nux_sheet_share_button"
+            ).click()
 
         print("uploaded reel")
+
+    def upload_post(self, d, path):
+        d.shell(
+            f"am start -a android.intent.action.SEND -t video/* --eu android.intent.extra.STREAM {path} com.instagram.android"
+        )
+        d(text="Feed").click()
+        time.sleep(2)
+
+        if d(resourceId="android:id/button_once").exists():
+            d(resourceId="android:id/button_once").click()
+        print("uploaded")
--- index.py
+++ index.py
@@ -1,7 +1,52 @@
 import time
 import uiautomator2 as u2
+from uiautomator2 import Device
+import random
 from gdrive_handler import GDriveHandler
 from file_uploader import FileUploader
+
+RANDOM_ACTIVITY_TIME = 30
+
+
+def random_activity(d: Device):
+    print(f"Performing random activity on device: {d.serial}")
+
+    # Click on home in case home isn't open
+    d(resourceId="com.instagram.android:id/feed_tab").click()
+    time.sleep(2)
+
+    # Perform random activity for a set time, scrolling or liking at random intervals
+    total_duration = RANDOM_ACTIVITY_TIME
+    probability = 0.5
+
+    # Get the screen size
+    screen_size = d.window_size()
+    screen_size_x = screen_size[0]
+    screen_size_y = screen_size[1]
+
+    # Start time of the loop
+    start_time = time.time()
+
+    # Main loop
+    while time.time() - start_time < total_duration:
+        # Generate random time interval and number
+        interval = random.uniform(5, 15)
+        random_number = random.random()
+        print(f"random number is {random_number}")
+
+        if random_number < probability:
+            # Swipe the screen from bottom to top
+            d.swipe(
+                screen_size_x / 2,
+                screen_size_y * 0.8,
+                screen_size_x / 2,
+                screen_size_y * 0.2,
+                0.05,
+            )
+
+        time.sleep(interval)
+
+    time.sleep(2)
 
 
 def main():
@@ -19,6 +64,8 @@ def main():
 
         android_vid_path = file_handler.transfer_file_to_device(d)
         file_handler.upload_reel(d, android_vid_path)
+
+        random_activity(d)
 
         time.sleep(1500)
 
