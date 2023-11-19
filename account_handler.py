d780276dcd304e433472c6bb60fa202aa9213919
Vir <73329256+Vir-8@users.noreply.github.com>
2023-11-19 19:00:45 +0530

    Implement multithreading to automate multiple devices concurrently

---
+++ account_handler.py
@@ -0,0 +1,58 @@
+import time
+
+resource_ids = {
+    "profile_button": "com.instagram.android:id/profile_tab",
+    "account_arrow": "com.instagram.android:id/action_bar_little_icon_container",
+    "account_element": "com.instagram.android:id/row_user_textview",
+}
+
+
+class AccountHandler:
+    def __init__(self, d):
+        self.d = d
+        pass
+
+    def get_all_accounts(self):
+        d = self.d
+
+        # Open instagram
+        d.app_start("com.instagram.android", activity=".activity.MainTabActivity")
+        time.sleep(2)
+
+        # Open profile page
+        d(resourceId=resource_ids["profile_button"]).click()
+        time.sleep(1)
+
+        # Open accounts menu
+        d(resourceId=resource_ids["account_arrow"]).click()
+
+        time.sleep(1)
+
+        # Get number of accounts
+        account_elements = d(resourceId=resource_ids["account_element"])
+        number_of_accounts = len(account_elements) - 1
+
+        accounts = [0] * number_of_accounts
+
+        for i in range(number_of_accounts):
+            element = account_elements[i]
+            name = element.info["text"]
+            accounts[i] = name
+
+        return accounts
+
+    def switch_account(self, account):
+        d = self.d
+
+        # Open profile page
+        d(resourceId=resource_ids["profile_button"]).click()
+        time.sleep(1)
+
+        # Open accounts menu
+        d(resourceId=resource_ids["account_arrow"]).click()
+
+        time.sleep(1)
+
+        # Get number of accounts
+        d(resourceId=resource_ids["account_element"], text=account).click()
+        time.sleep(3)
--- index.py
+++ index.py
@@ -2,9 +2,12 @@ import time
 import uiautomator2 as u2
 from uiautomator2 import Device
 import random
+import adbutils
 from gdrive_handler import GDriveHandler
 from file_uploader import FileUploader
 from growth_handler import GrowthHandler
+from account_handler import AccountHandler
+import concurrent.futures
 
 RANDOM_ACTIVITY_TIME = 60
 
@@ -53,32 +56,102 @@ def random_activity(d: Device):
     time.sleep(2)
 
 
-def main():
-    d = u2.connect()
-    print(d.dump_hierarchy())  # As UIAutomator2 can cause issues otherwise
+def split_content_chunks(connected_devices, videos):
+    # Get total number of accounts at the start to divide into chunks
+    total_number_of_accounts = 0
+    with concurrent.futures.ThreadPoolExecutor(
+        max_workers=len(connected_devices)
+    ) as executor:
+        # Create a separate instance for each device inside the thread
+        instances = [AccountHandler(device) for device in connected_devices]
 
+        # Use a lambda function to call get_all_accounts on each instance
+        results = list(
+            executor.map(
+                lambda instance: instance.get_all_accounts(),
+                instances,
+            )
+        )
+
+    # Calculate total number of videos and average videos per account
+    total_number_of_videos = len(videos)
+    total_number_of_accounts = sum(len(result) for result in results)
+    average_videos_per_account = total_number_of_videos / total_number_of_accounts
+
+    # Create video chunks based on the number of accounts on each device
+    content_chunks = []
+    for result in results:
+        num_videos_for_device = int(average_videos_per_account * len(result))
+        content_chunks.append(videos[:num_videos_for_device])
+        videos = videos[num_videos_for_device:]
+
+    return content_chunks
+
+
+def account_growth_handler(d):
+    account_handler = AccountHandler(d)
+    growth_handler = GrowthHandler(d)
+
+    accounts = account_handler.get_all_accounts()
+
+    for account in accounts:
+        account_handler.switch_account(account)
+        growth_handler.follow_accounts()
+        growth_handler.promote_accounts()
+        time.sleep(8)
+
+
+def content_upload_handler(d, chunk):
+    pc_folder_path = "./videos"
+    gdrive_handler = GDriveHandler(pc_folder_path)
+    account_handler = AccountHandler(d)
+
+    index = 0
+    accounts = account_handler.get_all_accounts()
+    num_of_accounts = len(accounts)
+    num_of_posts = len(chunk)
+
+    for content in chunk:
+        # Switch account for each iteration
+        current_account = accounts[index % num_of_accounts]
+        account_handler.switch_account(current_account)
+
+        pc_vid_path = gdrive_handler.download_files(content)
+        file_handler = FileUploader(pc_vid_path)
+
+        android_vid_path = file_handler.transfer_file_to_device(d)
+        file_handler.upload_reel(d, android_vid_path)
+
+        random_activity(d)
+        index += 1
+        num_of_posts -= 1
+        time.sleep(1500)
+
+
+def main():
     pc_folder_path = "./videos"
     gdrive_handler = GDriveHandler(pc_folder_path)
 
-    upload_videos = input("Do you want to upload videos? (true/false): ")
+    connected_devices = [
+        u2.connect(device.serial) for device in adbutils.adb.device_list()
+    ]
+    videos = gdrive_handler.get_all_videos()
+    choice = input("1. Upload videos\n2. Grow accounts\nEnter your choice: ")
 
-    if upload_videos == "true":
-        video_files = gdrive_handler.get_all_videos()
-
-        for video in video_files:
-            pc_vid_path = gdrive_handler.download_files(video)
-            file_handler = FileUploader(pc_vid_path)
-
-            android_vid_path = file_handler.transfer_file_to_device(d)
-            file_handler.upload_reel(d, android_vid_path)
-
-            random_activity(d)
-
-            time.sleep(1500)
+    if choice == "1":
+        content_chunks = split_content_chunks(connected_devices, videos)
+        with concurrent.futures.ThreadPoolExecutor(
+            max_workers=len(connected_devices)
+        ) as executor:
+            executor.map(content_upload_handler, connected_devices, content_chunks)
+    elif choice == "2":
+        with concurrent.futures.ThreadPoolExecutor(
+            max_workers=len(connected_devices)
+        ) as executor:
+            # Pass the instances as arguments to the threaded function
+            executor.map(account_growth_handler, connected_devices)
     else:
-        growth_handler = GrowthHandler(d)
-        growth_handler.follow_accounts()
-        growth_handler.promote_accounts()
+        print("Error! Invalid choice, please try again!")
 
 
 main()
