d99c651b7ecf3b7b62945ce726510f1d1e07ab95
Vir <73329256+Vir-8@users.noreply.github.com>
2023-11-20 22:48:19 +0530

    Clean code, fix bug causing failure to upload reels

--- file_uploader.py
+++ file_uploader.py
@@ -3,6 +3,15 @@ import time
 
 VIDEO_PATH_ON_DEVICE = "storage/emulated/0/Movies/"
 
+resource_ids = {
+    "just_once_button": "android:id/button_once",
+    "video_reel_popup_ok": "com.instagram.android:id/primary_action",
+    "reel_upload_next": "com.instagram.android:id/clips_right_action_button",
+    "reel_upload_share": "com.instagram.android:id/share_button",
+    "reel_privacy_popup_share_1": "com.instagram.android:id/clips_download_privacy_nux_button",
+    "reel_privacy_popup_share_2": "com.instagram.android:id/clips_nux_sheet_share_button",
+}
+
 
 class FileUploader:
     def __init__(self, file):
@@ -26,8 +35,8 @@ class FileUploader:
         d(text="Stories").click()
         time.sleep(2)
 
-        if d(resourceId="android:id/button_once").exists():
-            d(resourceId="android:id/button_once").click()
+        if d(resourceId=resource_ids["just_once_button"]).exists():
+            d(resourceId=resource_ids["just_once_button"]).click()
 
         print("uploaded")
 
@@ -37,53 +46,38 @@ class FileUploader:
         )
         if d(text="Reels").exists():
             d(text="Reels").click()
-        else:
+        elif d(description="Reels").exists():
             d(description="Reels").click()
 
-        if d(resourceId="android:id/button_once").exists():
-            d(resourceId="android:id/button_once").click()
+        if d(resourceId=resource_ids["just_once_button"]).exists():
+            d(resourceId=resource_ids["just_once_button"]).click()
         time.sleep(12)
 
         # Popup that says "videos will be uploaded as reels instead of posts"
-        if d(text="OK", resourceId="com.instagram.android:id/primary_action").exists():
-            d(text="OK", resourceId="com.instagram.android:id/primary_action").click()
+        if d(text="OK", resourceId=resource_ids["video_reel_popup_ok"]).exists():
+            d(text="OK", resourceId=resource_ids["video_reel_popup_ok"]).click()
             time.sleep(2)
 
         # Click next button
-        if d(
+        d(
             description="Next",
-            resourceId="com.instagram.android:id/clips_right_action_button",
-        ).exists():
-            d(
-                description="Next",
-                resourceId="com.instagram.android:id/clips_right_action_button",
-            ).click()
-            time.sleep(2)
-
+            resourceId=resource_ids["reel_upload_next"],
+        ).click()
         time.sleep(6)
 
         # upload the reel
-        if d(resourceId="com.instagram.android:id/share_button").exists():
-            d(resourceId="com.instagram.android:id/share_button").click()
+        if d(resourceId=resource_ids["reel_upload_share"]).exists():
+            d(resourceId=resource_ids["reel_upload_share"]).click()
             time.sleep(2)
 
         # Popup about privacy, reels being visible to others
-        if d(
-            resourceId="com.instagram.android:id/clips_download_privacy_nux_button"
-        ).exists():
-            d(
-                resourceId="com.instagram.android:id/clips_download_privacy_nux_button"
-            ).click()
+        if d(resourceId=resource_ids["reel_privacy_popup_share_1"]).exists():
+            d(resourceId=resource_ids["reel_privacy_popup_share_1"]).click()
             time.sleep(2)
-        elif d(
-            resourceId="com.instagram.android:id/clips_nux_sheet_share_button"
-        ).exists():
-            print("here")
-            d(
-                resourceId="com.instagram.android:id/clips_nux_sheet_share_button"
-            ).click()
+        elif d(resourceId=resource_ids["reel_privacy_popup_share_2"]).exists():
+            d(resourceId=resource_ids["reel_privacy_popup_share_2"]).click()
 
-        print("uploaded reel")
+        print("Uploaded reel.")
 
     def upload_post(self, d, path):
         d.shell(
@@ -92,6 +86,6 @@ class FileUploader:
         d(text="Feed").click()
         time.sleep(2)
 
-        if d(resourceId="android:id/button_once").exists():
-            d(resourceId="android:id/button_once").click()
+        if d(resourceId=resource_ids["just_once_button"]).exists():
+            d(resourceId=resource_ids["just_once_button"]).click()
         print("uploaded")
